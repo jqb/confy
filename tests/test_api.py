3cd16f493f3f6fe451d954f183d7e049bc0d6b02
jqb <kuba.janoszek@gmail.com>
2013-07-07 00:47:07 +0200

    module api simplest test added

---
+++ tests/test_api.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import unittest
+import confy
+
+from nose.tools import (
+    assert_in,
+)
+
+
+class ConfyModuleAPI(unittest.TestCase):
+    def test_has_all_required_elemnets(self):
+        expected = [
+            'VERSION',
+
+            # properties
+            'BaseProperty', 'ImporterProperty', 'InterpolationProperty',
+            'LazyProperty', 'RawProperty', 'ValueProperty',
+
+            # loader and all the falks
+            'Loader', 'loader', 'collection', 'env', 'lazyimport', 'lazy', 'raw',
+
+            # submodules
+            'utils', 'properties', 'api'
+        ]
+        module_contents = dir(confy)
+
+        for name in expected:
+            assert_in(name, module_contents)
+
