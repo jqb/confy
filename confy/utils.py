df3b338826a4df67ed92372d0a7375d594e2059a
jqb <kuba.janoszek@gmail.com>
2013-07-14 00:07:53 +0200

    utils: unused stuff removed

--- confy/utils.py
+++ confy/utils.py
@@ -63,38 +63,6 @@ def execfile(file_path, global_vars=None, local_vars=None):
     return global_vars, local_vars
 
 
-def flatten(*things, **kwargs):
-    flat_only_types = tuple(kwargs.get('flat_only') or ())
-    flatten = []
-
-    def item_and_item_type(item):
-        return item, type(item)
-
-    for m, m_type in map(item_and_item_type, things):
-        if not flat_only_types and issubclass(m_type, collections.Iterable):
-            flatten.extend(m)
-        elif issubclass(m_type, flat_only_types):
-            flatten.extend(m)
-        else:
-            flatten.append(m)
-
-    return flatten
-
-
-class OrderedSet(collections.OrderedDict):
-    def __init__(self, sequence=None):
-        super(OrderedSet, self).__init__([
-            (e, True) for e in (sequence or [])
-        ])
-
-    def add(self, elem):
-        self[elem] = True
-
-    def extend(self, sequence):
-        for elem in sequence:
-            self[elem] = True
-
-
 class Importer(object):
     def __init__(self, path):
         self.__path = path
