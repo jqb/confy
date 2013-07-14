5b989ecb6e767236d3ae1b0433663327e548e4bf
jqb <kuba.janoszek@gmail.com>
2013-07-15 00:09:09 +0200

    setup.py: classifiers fixed

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.2',
     ],
     packages=find_packages(exclude=("tests*",)),
     include_package_data = True,
