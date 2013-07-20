2a62bf60293def40df2bf5f95a1c59519037ff4e
jqb <kuba.janoszek@gmail.com>
2013-07-20 15:10:44 +0200

    setup.py: examples excluded from list of packages to install

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
     ],
-    packages=find_packages(exclude=("tests*",)),
+    packages=find_packages(exclude=("tests*", "examples*")),
     include_package_data = True,
     zip_safe=False,
     install_requires = [
