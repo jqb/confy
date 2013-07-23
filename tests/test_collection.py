efc491d5611a16dc276b84745814535fbc547c1a
jqb <kuba.janoszek@gmail.com>
2013-07-23 22:04:34 +0200

    collection: tests added

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -3,6 +3,19 @@ import unittest
 import confy
 
 
+class NonMapping(object):
+    def __init__(self):
+        self._data = {
+            'key': 'value',
+        }
+
+    def keys(self):
+        return self._data.keys()
+
+    def __getitem__(self, name):
+        return self._data[name]
+
+
 class CollectionFeatures(unittest.TestCase):
     def setUp(self):
         self.settings = confy.collection(
@@ -11,11 +24,16 @@ class CollectionFeatures(unittest.TestCase):
                 GET_OBJECT = '{root}/get',
                 ADD_OBJECT = '{root}/add',
                 REMOVE_OBJECT = '{root}/remove',
+                UNEXISTED_VARIABLE = '{unexisted}/hello!'
             ),
             FAKE_BACKEND_CLASS = confy.lazyimport('tests.fake.backend.FakeBackend'),
             FAKE_BACKEND_MODULE = confy.lazyimport('tests.fake.backend'),
             RAW_VALUE = confy.raw('{with "raw" I can put {} as many special things as I want}'),
-            URLS = confy.lazy(lambda self: "%s %s" % (self.API.GET_OBJECT, self.API.ADD_OBJECT))
+            URLS = confy.lazy(lambda self: "%s %s" % (self.API.GET_OBJECT, self.API.ADD_OBJECT)),
+
+            # rootpath works only in conjunction with __rootfile__ variable
+            __rootfile__ = '/path/to/project/settings/__init__.py',
+            PROJECT_ROOT = confy.rootpath('..'),
         )
 
     def test_variable_interpolation_should_work_as_a_charm(self):
@@ -25,6 +43,9 @@ class CollectionFeatures(unittest.TestCase):
         assert API.GET_OBJECT == 'http://api.veryfancy.com/get'
         assert API.ADD_OBJECT == 'http://api.veryfancy.com/add'
         assert API.REMOVE_OBJECT == 'http://api.veryfancy.com/remove'
+
+    def test_interpolation_for_variable_that_does_not_exists_raises_key_error(self):
+        self.assertRaises(KeyError, lambda: self.settings.API.UNEXISTED_VARIABLE)
 
     def test_lazyimport(self):
         from tests.fake.backend import FakeBackend
@@ -40,6 +61,9 @@ class CollectionFeatures(unittest.TestCase):
     def test_lazy_evaluation_should_work_as_a_charm(self):
         api = self.settings.API
         assert self.settings.URLS == "%s %s" % (api.GET_OBJECT, api.ADD_OBJECT)
+
+    def test_rootpath_constructs_relatively_correct_paths(self):
+        assert self.settings.PROJECT_ROOT == '/path/to/project'
 
 
 class CollectionDict(unittest.TestCase):
@@ -63,6 +87,17 @@ class CollectionDict(unittest.TestCase):
         assert value == 'http://something.com'
         assert 'URL' not in self.collection
 
+    def test_pop_should_raise_key_error(self):
+        self.assertRaises(KeyError, self.collection.pop, 'unexisted')
+
+    def test_popitem_should_return_name_with_value_tuple(self):
+        name, value = self.collection.popitem()
+        assert name not in self.collection
+
+    def test_popitem_raises_error_then_collection_is_empty(self):
+        self.collection.clear()
+        self.assertRaises(KeyError, self.collection.popitem)
+
     def test_keys(self):
         current = set(self.collection.keys())
         expected = set(['protocol', 'URL'])
@@ -75,3 +110,72 @@ class CollectionDict(unittest.TestCase):
 
     def test__eq__(self):
         assert self.collection == dict(self.collection)
+        assert self.collection.__eq__(NonMapping()) == False
+
+    def test__ne__(self):
+        data = dict(self.collection)
+        assert self.collection == data
+
+        data['protocol'] = 'https'
+        assert self.collection != data
+
+    def test_update_takes_exactly_1_postional_arguments(self):
+        self.assertRaises(TypeError, self.collection.update, {}, {})  # except self!
+
+        try:
+            self.collection.update({
+                'key': 'value',
+            })
+        except TypeError:
+            self.fail("Update should work with just one positional argument")
+
+    def test_update_works_also_with_non_mappings_and_iterables_of_tuples(self):
+        assert 'key' not in self.collection
+        self.collection.update(NonMapping())
+        assert 'key' in self.collection
+
+        assert 'key2' not in self.collection
+        self.collection.update([('key2', 'value2')])
+        assert 'key2' in self.collection
+
+    def test_setdefault(self):
+        assert 'protocol' in self.collection
+        result = self.collection.setdefault('protocol', 'value')
+        assert result == 'http'
+        assert 'protocol' in self.collection
+
+        assert 'unexisted' not in self.collection
+        result = self.collection.setdefault('unexisted', 'value')
+        assert result == 'value'
+        assert 'unexisted' in self.collection
+
+    def test_iterkeys(self):
+        assert list(self.collection.keys()) == list(self.collection.iterkeys())
+
+    def test_deleting_item(self):
+        del self.collection['protocol']
+        assert 'protocol' not in self.collection
+
+    def test_get(self):
+        assert 'key' not in self.collection
+        result = self.collection.get('key')
+        assert result is None
+
+        result = self.collection.get('key', 'default')
+        assert result == 'default'
+
+    def test_itervalues(self):
+        assert list(self.collection.values()) == list(self.collection.itervalues())
+
+    def test_iteritems(self):
+        assert list(self.collection.items()) == list(self.collection.iteritems())
+
+    def test_raw_items(self):
+        assert len(self.collection.raw_items()) == 2
+
+    def test_extend(self):
+        result = self.collection.extend({
+            'key': 'value'
+        })
+        assert result != self.collection
+        assert 'key' in result
